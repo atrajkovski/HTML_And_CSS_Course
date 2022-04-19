58547fe730f11b587944797569a03a8ff5157d30
atrajkovski <dsandra86@yahoo.com>
2022-04-19 13:41:10 +0200

    finished Forward Fitness Club

--- 10_CreatingInteractivityWithCSSAndJavaScript/Script.js
+++ 10_CreatingInteractivityWithCSSAndJavaScript/Script.js
@@ -3,35 +3,36 @@ Student Name: Aleksandra
 File Name: script.js
 Date: 14. 04. 2022
 */
-var videoSource = document.getElementById("vid-src");
+
+//Global variables
 var video = document.getElementById("example");
-var descriptionSource = document.getElementById("despsrc");
+var videoSource = document.getElementById("vid-src");
+var descriptionSource = document. getElementById("despsrc");
+video.style.width = "500px";
+video.style.height = "300px";
 
-// Hamburger menu function
-function hamburger() {
-    var menu = document.getElementById("menu-links");
-    var logo = document.getElementById("ffc-logo");
-    if (menu.style.display === "block" && logo.style.display === "none") {
-        menu.style.display = "none";
-        logo.style.display = "block";
-    }else {
-    menu.style.display = "block";
-    logo.style.display = "none";
-    }
+//Function to display  burpees  video
+function burpees() {
+    videoSource.src = "./media/burpees.mp4";
+   video.style.display = "block";
+   video.load();
 }
-
-//Function to display plank video
+​
+// Function to display plank video
 function plank() {
-    videoSource.src = "media/plank.mp4";
-     // descriptionSource.src = "media/plankdescriptions.vtt"; 
-     video.style.display = "block";
-     video.load();
+    videoSource.src = "./media/plank.mp4";
+    video.style.display = "block";
+    video.load();
+ }
+​
+ // Function to display climbers video
+function climbers() {
+    videoSource.src = "./media/climbers.mp4";
+    video.style.display = "block";
+    video.load();
  }
 
-// Function to display a promo code
-function discount() {
-    var promo = document.getElementById("special");
-    promo.firstChild.nodeValue = "Promo Code: D25START";
-    promo.style.color = "#ff0000"
-    promo.style.fontSize = "2em";
-}
+
+
+
+
