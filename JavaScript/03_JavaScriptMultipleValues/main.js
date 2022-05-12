e922e7522afb6416c1032de2201b778b13c24efb
atrajkovski <dsandra86@yahoo.com>
2022-05-12 15:17:17 +0200

    finished Javascript MultipleValues

---
+++ JavaScript/03_JavaScriptMultipleValues/main.js
@@ -0,0 +1,36 @@
+console.log("test");
+
+let arr = ["hi there", 5, true];
+console.log(arr);
+
+
+console.log("arr firts value: " + arr[0] + ". Is of type: " + typeof arr[0]);
+console.log("arr second value: " + arr[1] + ". Is of type: " + typeof arr[1]);
+console.log("arr third value: " + arr[2] + ". Is of type: " + typeof arr[2]);
+
+
+let arr2 = new Array(10);
+
+console.log(arr2);
+
+let colors = ["red", "green", "blue"];
+let boolean = [true, false, false, true];
+let emptyArr =[];
+
+console.log("The length of the colors array is:" + colors.length);
+
+console.log("-------------------------------");
+let arr10 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
+
+console.log(arr10.length);
+
+console.log(arr10[arr10.length -1]);
+
+let array = ["Milk", "Bread", "Apple"];
+
+console.log(array.length);
+array[1] = "Banana";
+
+console.log(array);
+
+
